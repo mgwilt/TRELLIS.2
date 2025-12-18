4562a5ce628b034300a4691b444b4fbfbe63a70f
mike <571232+mgwilt@users.noreply.github.com>
2025-12-18 14:29:58 -0800

    Update dependencies in requirements-docker.txt and setup.sh to replace pillow-simd with pillow version 10.4.0 for improved compatibility.

--- setup.sh
+++ setup.sh
@@ -80,7 +80,7 @@ if [ "$BASIC" = true ] ; then
     pip install imageio imageio-ffmpeg tqdm easydict opencv-python-headless ninja trimesh transformers gradio==6.0.1 tensorboard pandas lpips zstandard
     pip install git+https://github.com/EasternJournalist/utils3d.git@9a4eb15e4021b67b12c460c7057d642626897ec8
     sudo apt install -y libjpeg-dev
-    pip install pillow-simd
+    pip install pillow==10.4.0
     pip install kornia timm
 fi
 
